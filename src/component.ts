06345841652746cb631f39848580e21f9186d998
Adam Duren <adam@wearenorthern.com>
2016-08-09 20:58:51 -0400

    Initial test commit

---
+++ src/component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'custom-component',
+  templateUrl: 'component.html',
+})
+export class CustomComponent {}
